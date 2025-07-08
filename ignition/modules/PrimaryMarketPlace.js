e81115d248a00ca33a31979a3a14d5f3876286d7
dhananjaypai08 <dhananjay2002pai@gmail.com>
2025-07-08 23:46:49 +0530

    fix(dev-ui & contracts): fixing dev-ui for secondary marketplace and secondary marketplace logic
    fixing dev-ui and changing and redeploying secondary marketplace

--- ignition/modules/PrimaryMarketPlace.js
+++ ignition/modules/PrimaryMarketPlace.js
@@ -5,7 +5,7 @@ module.exports = buildModule("PrimaryMarketPlaceModule", (m) => {
   const PrimaryMarketPlaceModule = m.contract("PrimaryMarketPlace", [
     "0x1d72B383cd2F783e4f2eDafE9D7544A3355507C2",
     "0x1d72B383cd2F783e4f2eDafE9D7544A3355507C2",
-    "0xFbE36b09339adbA36824e4b966214F2F298A3716"
+    "0xC82a815602E1552347bb29944e5Fe44a4da438Fb"
   ]);
 
   return { PrimaryMarketPlaceModule };
