cf1435b50c907ea1b26841e386f3d2fb4cb0dceb
dhananjaypai08 <dhananjay2002pai@gmail.com>
2025-08-01 14:25:14 +0530

    chore(rm-ignition): remove ignition module from current foundry project
    removing hardhat dependency from current foundry project

--- ignition/modules/LicenseContract.js
+++
@@ -1,8 +0,0 @@
-const { buildModule } = require("@nomicfoundation/hardhat-ignition/modules");
-
-module.exports = buildModule("LicenseContractModule", (m) => {
-
-  const LicenseContractModule = m.contract("LicenseContract");
-
-  return { LicenseContractModule };
-});
