2c80a04d42083d3db366b8262a81ef360571fcbc
dhananjaypai08 <dhananjay2002pai@gmail.com>
2025-09-11 12:30:06 +0530

    fix(re-add-seller): re-adding tokenId to primary marketplace's offer.seller
    re-adding tokenId to primary marketplace's offer.seller

--- test/SecondaryMarketPlace.t.sol
+++ test/SecondaryMarketPlace.t.sol
@@ -3,10 +3,12 @@ pragma solidity ^0.8.28;
 
 import {BaseSetup} from "./BaseSetup.t.sol";
 import {ILicenseContract} from "src/interfaces/ILicenseContract.sol";
+import {IPrimaryMarketPlace} from "src/interfaces/IPrimaryMarketPlace.sol";
 import {
     insufficientPayment,
     cannotBuyYourOwnOffer,
-    ContractNotOwner
+    ContractNotOwner,
+    LicenseAddressDifferent
 } from "src/errors/SecondaryMarketPlace.sol";
 
 contract SecondaryMarketPlaceTest is BaseSetup {
@@ -89,5 +91,53 @@ contract SecondaryMarketPlaceTest is BaseSetup {
         vm.prank(buyer);
         vm.expectRevert(abi.encodeWithSelector(ContractNotOwner.selector));
         secondary.removeOffer(tokenId);
+    }
+
+    function testCoordinatorRemoveOfferReturnsToSellerAndUpdatesPrimary() public {
+        (address licenseAddr, , uint256 tokenId) = _mintForSecondary();
+        vm.prank(buyer);
+        ILicenseContract(licenseAddr).approve(address(secondary), tokenId);
+        vm.prank(buyer);
+        secondary.createOffer(tokenId, licenseAddr, 1 ether);
+
+        vm.prank(coordinator);
+        secondary.removeOffer(tokenId);
+
+        assertEq(
+            ILicenseContract(licenseAddr).ownerOf(tokenId),
+            buyer,
+            "NFT not returned to seller on coordinator cancel"
+        );
+
+        IPrimaryMarketPlace.GameNft memory nftData = primary.getNftDetails(tokenId);
+        assertEq(nftData.listedForSale, false, "listedForSale not reset");
+        assertEq(nftData.owner, buyer, "primary owner not reset to seller");
+
+        uint256[] memory userIds = primary.getUserNftIds(buyer);
+        bool found;
+        for (uint256 i = 0; i < userIds.length;) {
+            if (userIds[i] == tokenId) {
+                found = true;
+                break;
+            }
+            unchecked{
+                ++i;
+            }
+        }
+        assertTrue(found, "tokenId not re-added to seller's user list");
+    }
+
+    function testCreateOfferWrongLicenseAddressReverts() public {
+        (address licenseAddr, , uint256 tokenId) = _mintForSecondary();
+        (address wrongLicenseAddr, ) = _createLicense(true);
+
+        vm.prank(buyer);
+        ILicenseContract(licenseAddr).approve(address(secondary), tokenId);
+
+        vm.prank(buyer);
+        vm.expectRevert(
+            abi.encodeWithSelector(LicenseAddressDifferent.selector, wrongLicenseAddr)
+        );
+        secondary.createOffer(tokenId, wrongLicenseAddr, 1 ether);
     }
 }
